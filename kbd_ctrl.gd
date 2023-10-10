c7c31a0350281d39fede59238dfa869293c33c08
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-10-10 12:44:01 -0500

    Update 10/10

--- kbd_ctrl.gd
+++ kbd_ctrl.gd
@@ -5,7 +5,23 @@ extends Node2D
 func _ready():
 	$world/CollisionPolygon2D/Polygon2D.polygon = $world/CollisionPolygon2D.polygon
 
+func _on_interact_mouse_entered():
+	print("Hovered")
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		$interact.queue_free()  # Delete from world
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var dragging = false
+var offset = Vector2.ZERO
+
 func _process(delta):
-	pass
+	if dragging:
+		$draggable.global_position = get_global_mouse_position() - offset
+
+func _on_button_button_down():
+	dragging = true
+	offset = get_global_mouse_position() - $draggable.global_position
+
+func _on_button_button_up():
+	dragging = false
+
+
