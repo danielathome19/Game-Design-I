052545cd9c055bb9c5b56de727a4cd1ec579660a
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-01-22 08:30:08 -0600

    Removed old semester

--- entities/mini_heart.gd
+++
@@ -1,8 +0,0 @@
-extends BaseItem
-
-func _init():  super._init(5)
-func _ready(): super._ready()
-
-func interact(player):
-	player.pickup_health(value)
-	remove()
--- kbdexit.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var next_level = ""
-
-func _on_body_entered(body):
-	if body.name == "kbdplayer":
-		OS.alert("You won!")
-		var lvl = "res://" + next_level + ".tscn"
-		get_tree().change_scene_to_file(lvl)
