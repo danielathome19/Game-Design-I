4c6d1a1119a7c6afc893197f648fbbf48cdbfe5e
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-01-29 13:17:39 -0600

    Update 1/29

---
+++ Prog82a.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_btn_calc_pressed():
+	var speedLimit = int($txtLimit.text)
+	var carSpeed = int($txtSpeed.text)
+	var milesOver = carSpeed - speedLimit
+	var fine = 20.0 + (milesOver * 5.0)
+	$lblOut.text = "Fine: $ %.2f" % fine
+	
+func _on_btn_exit_pressed():
+	get_tree().quit()
