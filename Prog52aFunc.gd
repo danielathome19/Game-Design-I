b9d6d3285f9a1ec32707d3a9c3883553959bece6
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-02-12 13:20:02 -0600

    Update 2/12

---
+++ Prog52aFunc.gd
@@ -0,0 +1,22 @@
+extends Control
+
+func sayHi():
+	print("Hi!")
+
+func getArea(length, width):
+	var area = length * width
+	return area  # Send value back out of function
+
+func getPerim(length, width):
+	return 2 * length + 2 * width
+
+func _on_btn_calc_pressed():
+	sayHi()
+	var l = int($txtLen.text)
+	var w = int($txtWid.text)
+	var a = getArea(l, w)
+	var p = getPerim(l, w)
+	$lblOut.text = "Area: %d\nPerimeter: %d" % [a, p]
+	
+
+
