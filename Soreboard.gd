a6c37f0d327b3853de610442f37c412fc69206a9
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-02-13 13:21:34 -0600

    Update 2/13

---
+++ Soreboard.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var score = 0
+
+func addPoint(): score += 1
+func subPoint(): score -= 1
+func reset():    score = 0
+
+func update():
+	$lblOut.text = "Score: %d" % score  # "Score: " + str(score)
+
+func _on_btn_update_pressed():
+	var choice = $LineEdit.text.to_lower()
+	if choice == "add":
+		addPoint()
+	elif choice == "sub" or choice == "subtract":  # elif "sub" in choice
+		subPoint()
+	else:
+		OS.alert("Invalid choice!")  # Capital O and S
+	update()
+
+func _on_btn_reset_pressed():
+	reset()
+	update()
+
