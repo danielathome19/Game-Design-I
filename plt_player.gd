461e58cf7d31c018e11f6505032be9a7483919db
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-02-21 13:37:49 -0600

    Update 2/21

--- plt_player.gd
+++ plt_player.gd
@@ -25,4 +25,17 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	update_animation(direction)  # Update animation based on state
+	
 	move_and_slide()
+
+
+@onready var anim = $AnimatedSprite2D
+func update_animation(direction):
+	if not is_on_floor():  # Jumping
+		anim.play("jump")
+	elif direction != 0:  # Walking
+		anim.play("walk")
+		anim.flip_h = direction < 0  # Flip sprite if moving left
+	else:  # Idle
+		anim.play("default")
