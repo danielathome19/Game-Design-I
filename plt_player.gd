74af11378c98696ed062e2774b421138b0be9662
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-02-22 13:26:12 -0600

    Update 2/22

--- plt_player.gd
+++ plt_player.gd
@@ -6,11 +6,11 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var gravity_on = true
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and gravity_on:
 		velocity.y += gravity * delta
 
 	# Handle Jump.
@@ -26,6 +26,25 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	update_animation(direction)  # Update animation based on state
+	
+	if Input.is_action_just_pressed("ui_home"):
+		self.scale *= 2
+	if Input.is_action_just_pressed("ui_end"):
+		self.scale /= 2
+	if Input.is_action_just_pressed("ui_page_up"):
+		self.rotation_degrees += 5
+	if Input.is_action_just_pressed("ui_page_down"):
+		self.rotation_degrees -= 5
+	if Input.is_action_just_pressed("ui_text_completion_replace"):  # Tab
+		self.position.x += 500
+	if Input.is_action_just_pressed("ui_text_backspace"):
+		self.position.x -= 500
+	if Input.is_action_just_pressed("ui_text_toggle_insert_mode"):  # Insert
+		self.position.y += JUMP_VELOCITY / 2.0
+	if Input.is_action_just_pressed("ui_text_delete"):
+		self.position.y -= JUMP_VELOCITY / 2.0
+	if Input.is_action_just_pressed("ui_cancel"):  # Escape
+		gravity_on = not gravity_on
 	
 	move_and_slide()
 
