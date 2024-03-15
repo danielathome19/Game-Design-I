58b7f97eaa6f91c8f5374c1d52283ebfb0c11dcc
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-03-15 13:43:11 -0500

    Update 3/15

---
+++ player_hud.gd
@@ -0,0 +1,53 @@
+extends CanvasLayer
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+@onready var hearts = $PlayerHealth/Hearts
+
+const HEART_ROW_SIZE = 10
+const HEART_OFFSET = 16
+
+func create_heart():
+	var n_heart = Sprite2D.new()
+	n_heart.texture = hearts.texture
+	n_heart.hframes = hearts.hframes
+	n_heart.vframes = hearts.vframes
+	n_heart.frame = 8
+	hearts.add_child(n_heart)
+
+
+func draw_hearts():
+	for heart in hearts.get_children():
+		hearts.remove_child(heart)
+	for i in range(int(player.data.max_health) / 20):
+		create_heart()  # 1 heart per 20 hp
+
+func _ready():
+	draw_hearts()
+
+func _process(delta):
+	var p_health = player.data.health
+	var full_hearts = floor(p_health / 20)
+	var remainder = int(p_health) % 20
+	
+	for heart in hearts.get_children():
+		var index = heart.get_index()
+		var x = (index % HEART_ROW_SIZE) * HEART_OFFSET
+		var y = (index / HEART_ROW_SIZE) * HEART_OFFSET
+		heart.position = Vector2(x, y)
+		
+		# Frame 8 empty, 7 1/4, 6 half, 5 3/4, 4 full
+		if index > full_hearts:
+			heart.frame = 8
+		elif index == full_hearts:
+			heart.frame = 8 - int(remainder / 5)
+		elif index < full_hearts:
+			heart.frame = 4
+	pass
+
+
+
+
+
+
+
+
--- td_player.gd
+++ td_player.gd
@@ -1,13 +1,35 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
+const MAXIMUM_OBTAINABLE_HEALTH = 400.0
+
+enum STATES { IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING }
+
+@export var data = {
+	"max_health": 60.0,  # 20hp per heart; 5 per fraction
+	"health": 60.0,      # Min 60 Max 400
+	"money": 0,
+	"state": STATES.IDLE,
+	"secondaries": [],
+}
+
 var inertia = Vector2()
 var look_direction = Vector2.DOWN  # (0, 1)
 
 var menu_scene = preload("res://my_gui.tscn")
 var menu_instance = null
 
+@onready var p_HUD = get_tree().get_first_node_in_group("HUD")
+
+func pickup_money(value):
+	data.money += value
+
+func pickup_health(value):
+	data.health += value
+	data.health = clamp(data.health, 0, data.max_health)
+
 func _ready():
+	p_HUD.show()
 	menu_instance = menu_scene.instantiate()
 	$Camera2D.add_child.call_deferred(menu_instance)
 	menu_instance.hide()
