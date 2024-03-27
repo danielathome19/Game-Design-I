906bd1d30ac0516f71a6cb4d0974b7b04f55002d
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-03-27 13:34:12 -0500

    Update 3/27

--- td_player.gd
+++ td_player.gd
@@ -15,11 +15,36 @@ enum STATES { IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING }
 
 var inertia = Vector2()
 var look_direction = Vector2.DOWN  # (0, 1)
+var attack_direction = Vector2.DOWN
+var animation_lock = 0.0  # Lock player while playing attack animation
+var damage_lock = 0.0
+var charge_time = 2.5
+var charge_start_time = 0.0
 
+var slash_scene = preload("res://entities/attacks/slash.tscn")
 var menu_scene = preload("res://my_gui.tscn")
 var menu_instance = null
 
 @onready var p_HUD = get_tree().get_first_node_in_group("HUD")
+
+func get_direction_name():
+	return ["right", "down", "left", "up"][
+		int(round(look_direction.angle() * 2 / PI)) % 4
+	]
+
+func attack():
+	data.state = STATES.ATTACKING
+	var dir_name = get_direction_name()
+	if dir_name == "left":
+		$AnimatedSprite2D.flip_h = 0
+	$AnimatedSprite2D.play("swipe_" + dir_name)
+	attack_direction = look_direction
+	var slash = slash_scene.instantiate()
+	slash.position = attack_direction * 20.0
+	slash.rotation = Vector2().angle_to_point(-attack_direction)
+	add_child(slash)
+	animation_lock = 0.2
+	
 
 func pickup_money(value):
 	data.money += value
@@ -35,21 +60,33 @@ func _ready():
 	menu_instance.hide()
 
 func _physics_process(delta):
-	var direction = Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	)
-	update_animation(direction)
-	if direction.length() > 0:
-		look_direction = direction
-		# Scale to 1 to prevent speed boost from diagonals
-		direction = direction.normalized()
-		velocity = direction * SPEED
-	else:
-		velocity = velocity.move_toward(Vector2(), SPEED)
-	velocity += inertia
-	move_and_slide()
-	inertia = inertia.move_toward(Vector2(), delta * 1000.0)
+	animation_lock = max(animation_lock-delta, 0.0)
+	damage_lock = max(damage_lock-delta, 0.0)
+	
+	if animation_lock == 0.0 and data.state != STATES.DEAD:
+		if data.state != STATES.CHARGING:
+			data.state = STATES.IDLE
+		
+		var direction = Vector2(
+			Input.get_axis("ui_left", "ui_right"),
+			Input.get_axis("ui_up", "ui_down")
+		)
+		update_animation(direction)
+		if direction.length() > 0:
+			look_direction = direction
+			# Scale to 1 to prevent speed boost from diagonals
+			direction = direction.normalized()
+			velocity = direction * SPEED
+		else:
+			velocity = velocity.move_toward(Vector2(), SPEED)
+		velocity += inertia
+		move_and_slide()
+		inertia = inertia.move_toward(Vector2(), delta * 1000.0)
+	
+	if data.state != STATES.DEAD:
+		if Input.is_action_just_pressed("ui_accept"):
+			attack()
+			# TODO: charge timer/state
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
