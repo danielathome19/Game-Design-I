2ea613256bdfd5711331466aa2ce0f6e0bf73d6e
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-03-28 13:39:08 -0500

    Update 3/28

--- td_player.gd
+++ td_player.gd
@@ -44,7 +44,26 @@ func attack():
 	slash.rotation = Vector2().angle_to_point(-attack_direction)
 	add_child(slash)
 	animation_lock = 0.2
-	
+
+func charged_attack():
+	data.state = STATES.ATTACKING
+	$AnimatedSprite2D.play("swipe_charge")
+	attack_direction = look_direction
+	damage_lock = 0.3
+	for i in range(9):
+		# Offset by (i-4) * 45 degrees
+		var angle = -attack_direction.angle() + (i-4) * PI / 4  # [-4,4]
+		var dir = Vector2(cos(angle), sin(angle))
+		var slash = slash_scene.instantiate()
+		slash.position = dir * 20
+		slash.rotation = Vector2().angle_to_point(-dir)
+		slash.damage *= 1.5
+		add_child(slash)
+		await get_tree().create_timer(0.03).timeout
+	animation_lock = 0.2
+	await $AnimatedSprite2D.animation_finished
+	data.state = STATES.IDLE
+	pass
 
 func pickup_money(value):
 	data.money += value
@@ -86,7 +105,16 @@ func _physics_process(delta):
 	if data.state != STATES.DEAD:
 		if Input.is_action_just_pressed("ui_accept"):
 			attack()
-			# TODO: charge timer/state
+			charge_start_time = Time.get_time_dict_from_system().second
+			data.state = STATES.CHARGING
+		
+		if Input.is_action_just_released("ui_accept"):
+			var cur_time = Time.get_time_dict_from_system().second
+			var charge_duration = cur_time - charge_start_time
+			if charge_duration >= charge_time and data.state == STATES.CHARGING:
+				charged_attack()
+			else:
+				data.state = STATES.IDLE
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
@@ -94,20 +122,22 @@ func _physics_process(delta):
 
 
 func update_animation(direction):
-	var a_name = "idle_"
-	if direction.length() > 0:
-		look_direction = direction
-		a_name = "walk_"
-	if look_direction.x != 0:
-		a_name += "side"
-		$AnimatedSprite2D.flip_h = look_direction.x < 0
-	elif look_direction.y < 0:
-		a_name += "up"
-	elif look_direction.y > 0:
-		a_name += "down"
-		
-	$AnimatedSprite2D.animation = a_name
-	$AnimatedSprite2D.play()
+	if data.state == STATES.IDLE:
+		var a_name = "idle_"
+		if direction.length() > 0:
+			look_direction = direction
+			a_name = "walk_"
+		if look_direction.x != 0:
+			a_name += "side"
+			$AnimatedSprite2D.flip_h = look_direction.x < 0
+		elif look_direction.y < 0:
+			a_name += "up"
+		elif look_direction.y > 0:
+			a_name += "down"
+			
+		$AnimatedSprite2D.animation = a_name
+		$AnimatedSprite2D.play()
+	pass
 
 
 
