5396ec5269cce5e4333e62eed2ef3eff1d41f302
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-10-29 14:11:11 -0500

    Update 10/29

---
+++ high_scores.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var highscores = [28, 7, 15]
+
+func update_scoreboard():
+	$ItemList.clear()
+	highscores.sort()
+	# For-each loop
+	# for score in highscores:
+	# 	$ItemList.add_item(str(score))
+	for index in range(len(highscores)):
+		$ItemList.add_item("%d. %d" % [index+1, highscores[index]])
+
+func _ready() -> void:
+	update_scoreboard()
+
+func _on_button_pressed() -> void:
+	var score = int($LineEdit.text)
+	highscores.append(score)
+	update_scoreboard()
+
+func _on_button_2_pressed() -> void:
+	var position = int($LineEdit2.text) - 1
+	highscores.remove_at(position)
+	update_scoreboard()
