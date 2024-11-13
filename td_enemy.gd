34b664649e72c0c0faac545639e0b05ce7c29fde
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-11-13 14:08:21 -0600

    Update 11/13

--- td_enemy.gd
+++ td_enemy.gd
@@ -51,6 +51,31 @@ signal recovered
 @onready var rcL = $RayCast2DL
 @onready var anim_player = $AnimatedSprite2D
 
+var drops = ["drop_coin", "drop_heart"]
+var coin_scene = preload("res://entities/items/mini_coin.tscn")
+var heart_scene = preload("res://entities/items/mini_heart.tscn")
+
+func vec2_offset():
+	return Vector2(randf_range(-10.0, 10.0), randf_range(-10.0, 10.0))
+
+func drop_scene(item_scene):
+	item_scene.global_position = self.global_position + vec2_offset()
+	get_tree().current_scene.add_child(item_scene)
+
+func drop_heart():
+	drop_scene(heart_scene.instantiate())
+
+func drop_coin():
+	var coin = coin_scene.instantiate()
+	coin.value = self.money_value
+	drop_scene(coin)
+
+func drop_items():
+	var num_drops = randi() % 3 + 1
+	for i in range(num_drops):
+		var rnd_drop = drops[randi() % drops.size()]
+		call_deferred(rnd_drop)
+
 func turn_toward_player_location(location: Vector2):
 	# Set the state to move toward the player
 	var dir_to_player = (location - self.global_position).normalized()
@@ -75,7 +100,7 @@ func take_damage(dmg, attacker=null):
 		animation_lock = 0.2
 		# TODO: damage shader
 		if HEALTH <= 0:
-			# TODO: drop item
+			drop_items()
 			# TODO: play death sound
 			queue_free()
 		else:
